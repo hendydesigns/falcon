021b843925616ff2e9108043805c39a657036088
mlhender <mlhender@andrew.cmu.edu>
2019-07-23 20:10:23 -0400

    site

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+
+
+var swiper = new Swiper(".swiper-container", {
+  centeredSlides: true,
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar"
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
+
+swiper.on('slideChange', function () {
+
+  if(swiper.activeIndex == 0) {
+    $(".background").removeClass("active");
+    $(".one").addClass("active");
+  }
+  
+  if(swiper.activeIndex == 1) {
+    $(".background").removeClass("active");
+    $(".two").addClass("active");
+  }
+
+  if(swiper.activeIndex == 2) {
+    $(".background").removeClass("active");
+    $(".three").addClass("active");
+  }
+});
+
+window.onload = function() {
+  $(".featured").addClass("active");
+  $(".one").addClass("active");
+  $(".title").addClass("active");
+}
