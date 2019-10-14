93ac5398e6c8ce1848c249cf56da89b4f8cc6972
mlhender <mlhender@andrew.cmu.edu>
2019-10-13 20:44:59 -0400

    portfolio added

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,3 +1,5 @@
+// Portfolio swiper code
+
 var swiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: false,
@@ -16,4 +18,42 @@ var swiper = new Swiper(".swiper-container", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
   }
+});
+
+
+// Open and close gallerys
+$(".swiper-slide").each(function(index) {
+    $(this).on("click", function(){
+      $(".Gallery-wrapper").addClass("open");
+
+      $(".photo").remove();
+
+      //AJAX
+      var folder = "portfolio/project-" + (index+1) + "/";
+
+
+      $(document).ready(function () {
+        $.ajax({
+          url : folder,
+          success: function (data) {
+              $(data).find("a").attr("href", function (i, val) {
+                  if( val.match(/\.(jpe?g|png|gif)$/) ) {
+                      $(".photos-container").append( "<img class='photo' src='"+ folder + val +"'>" );
+                  }
+              });
+          }
+        });
+      });
+      //End add file
+
+
+    });
+});
+
+
+$(".close").click(function() {
+  $(".Gallery-wrapper").removeClass("open");
+  $(".photos-container").animate({
+        scrollTop: 0
+  }, 'slow');
 });
